67314872b710e879f187b3d15bc485482b446bf6
Fiza Hussain <fiza16hussain@gmail.com>
2023-03-22 13:58:28 +0530

    added css to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,37 +1,45 @@
-import "./Header.css"
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import DiamondIcon from '@mui/icons-material/Diamond';
-import SearchIcon from '@mui/icons-material/Search';
+import "./Header.css";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import DiamondIcon from "@mui/icons-material/Diamond";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function Header() {
   return (
     // header-div
     <div className="header">
-        {/* nav logo */}
+      {/* nav logo */}
       <div className="header-logo">
-      <DiamondIcon className="header-logo-img" fontSize="large"/>
-      <h2 className="header-logo-title">Bookish</h2>
+        <DiamondIcon className="header-logo-img" fontSize="large" />
+        <h2 className="header-logo-title">Bookish</h2>
       </div>
       {/* nav search */}
       <div className="header-search">
-        <input type="text" className="header-search-input" />
-        <SearchIcon className="header-search-icon"/>
+        <input
+          type="search"
+          className="header-search-input"
+          placeholder="search"
+        />
+        <button className="header-search-button">
+          <SearchIcon className="header-search-icon" />
+        </button>
       </div>
       {/* nav final comp */}
       <div className="header-nav">
         <div className="nav-item">
           <span className="1st-line">Hello User</span>
+          <br />
           <span className="2nd line">Sign in...</span>
         </div>
         <div className="nav-item">
           <span className="1st-line">Your</span>
+          <br />
           <span className="2nd line">Shop</span>
         </div>
-        <div className="nav-item">
-        <ShoppingBasketIcon />
+        <div className="nav-item basket-icon">
+          <ShoppingBasketIcon />
           <span className="2nd line">0</span>
         </div>
       </div>
     </div>
-  ) 
-};
+  );
+}
