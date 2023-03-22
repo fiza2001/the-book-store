4e4e0b95a79c4a2ad8b08d783e2ee3d42e192435
Fiza Hussain <fiza16hussain@gmail.com>
2023-03-22 09:48:55 +0530

    html elements and attributes added in app.js, and installed material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import "./App.css";
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function App() {
   return (
     <div className="App">
-      
+      {/* nav logo */}
+      <div className="header-logo"></div>
+      {/* nav search */}
+      <div className="header-search"></div>
+      {/* nav final comp */}
+      <div className="header-nav">
+        <div className="nav-item">
+          <span className="1st-line">Hello User</span>
+          <span className="2nd line">Sign in...</span>
+        </div>
+        <div className="nav-item">
+          <span className="1st-line">Your</span>
+          <span className="2nd line">Shop</span>
+        </div>
+        <div className="nav-item">
+        <ShoppingBasketIcon />
+          <span className="2nd line">0</span>
+        </div>
+      </div>
     </div>
   );
 }
