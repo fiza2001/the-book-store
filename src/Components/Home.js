f64f8b10f943cbd2fd152856a4b74f9bb65ccbb7
Fiza Hussain <fiza16hussain@gmail.com>
2023-03-23 15:14:00 +0530

    added values to homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,15 +9,52 @@ export default function Home() {
         <img src={require("./images/bk1.jpg")} alt="" className="home-img" />
         <div className="home-row">
           <div className="row-one">
-            <Product />
-            <Product />
+            <Product 
+            id="1"
+            title="I FELL IN LOVE WITH HOPE by LANCALI"
+            price={290}
+            description="Against the unforgiving landscape of a hospital...."
+            image="https://therightbookstoreindia.com/wp-content/uploads/2022/11/1669297396-1024x944.jpg"
+            />
+            <Product 
+             id="2"
+             title="IT STARTS WITH US by COLLEEN HOOVER"
+             price={190}
+             description="Before It Ends with Us, it started with Atlas...."
+             image="https://therightbookstoreindia.com/wp-content/uploads/2022/10/1666607725.jpg"
+             />
           </div>
           <div className="row-two">
-          <Product />
-          <Product />
-          <Product />
+          <Product 
+           id="3"
+           title="IKIGAI [HARDCOVER] by HECTOR GARCIA"
+           price={566}
+           description="It’s the Japanese word for ‘a reason to live’ or ‘a reason to jump out of bed in the morning’."
+           image="https://therightbookstoreindia.com/wp-content/uploads/2021/09/1632729563.jpg"
+           />
+          <Product 
+           id="4"
+           title="RICH DAD POOR DAD by ROBERT KIYOSAKI"
+           price={299}
+           description="Rich Dad Poor Dad is Robert’s story of growing up with two dads ..."
+           image="https://therightbookstoreindia.com/wp-content/uploads/2021/09/1632727603.jpg"
+           />
+          <Product 
+           id="5"
+           title="BEFORE YOUR MEMORY FADES by TOSHIKAZU KAWAGUCHI"
+           price={320}
+           description="On the hillside of Mount Hakodate in northern Japan, Cafe Donna Donna is fabled "
+           image="https://therightbookstoreindia.com/wp-content/uploads/2022/10/1666866230.jpg"
+           />
           </div>
-          <div className="row-three"><Product /></div>
+          <div className="row-three">
+          <Product 
+           id="6"
+           title="BERSERK MANGA VOL 1 KENTARO MIURA"
+           price={499}
+           description="His name is Guts, the Black Swordsman, a feared warrior spoken of only in whispers..."
+           image="https://therightbookstoreindia.com/wp-content/uploads/2023/01/1673280541.jpg"
+           /></div>
         </div>
       </div>
     </div>
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,16 +1,16 @@
 import "./Product.css";
 
-export default function Product() {
+export default function Product({id,title,price,description,image}) {
   return (
     <div>
       <div className="product">
         <div className="product-img-div">
-        <img src={require("./images/bk2.jpg")} alt="" className="product-img"/>
+        <img src={image} alt="" className="product-img"/>
         </div>
         <div className="product-info">
-          <h4>title (Fiction)</h4>
-          <h5>200 $</h5>
-          <p>lorem ipsum berla balado kavalri dhaja dhbshb.loplo asderf jsnjankmnanskamns</p>
+          <h4>{title}(Fiction)</h4>
+          <h5>{price}$</h5>
+          <p>{description}</p>
           <button>Add to cart</button>
         </div>
       </div>
